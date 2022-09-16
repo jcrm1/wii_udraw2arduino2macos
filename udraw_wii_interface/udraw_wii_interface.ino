f85021f18fe38366fe99746f3144f70f4130dad7
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2022-09-16 10:06:30 -0400

    Modified to emulate WACOM IV
    Modified to emulate a barebones implementation of the WACOM IV protocol

--- udraw_wii_interface/udraw_wii_interface.ino
+++ udraw_wii_interface/udraw_wii_interface.ino
@@ -1,6 +1,9 @@
+//#include <AbsMouse.h>
+
 //  ii_udraw2arduino2linux
 //  The Arduino portion of the code
-//  By Matthew Watts 2018
+//  Modified to emulate WACOM IV by jcrm1
+//  Created by Matthew Watts 2018
 //
 //  Tablet wiring differs from normal nunchuck wiring
 //  Yellow > A5/SCL
@@ -18,14 +21,17 @@
 // global most up to date tablet state
 uint16_t x_raw;
 uint16_t y_raw;
+//uint16_t x_raw_old = 0;
+//uint16_t y_raw_old = 0;
 uint8_t pressure_raw;
 uint8_t stylusb2;
 uint8_t stylusb1;
 uint8_t stylusb0;
-
+bool sending = true;
 // Setup serial and send init to tablet
 void setup() {
-  Serial.begin(38400);
+//  AbsMouse.init(5120, 4880);
+  Serial.begin(9600);
   Wire.begin();
   Wire.beginTransmission(0x52);
   Wire.write(0xF0);
@@ -102,16 +108,17 @@ void getDataFromTablet() {
 // only mode 0 is implemented here
 // but there is room to add modes later
 // for ISDV4 serial tablet emulation
-uint8_t serialMode=0;
+uint8_t serialMode=2;
 void sendData() {
-   // outgoing data
-  switch(serialMode) {
-     // normal mode spits out space-delimited raw x, y, pressure, button 0, 1, 2 then newline
-    // only sends data if something has changed
-    case 0:
-      if( hasChange == 0 ) {
-        return;
-      }
+  if (serialMode == 0) {
+      Serial.write(x_raw);
+      Serial.write(y_raw);
+      Serial.write(pressure_raw);
+      Serial.write(stylusb0);
+      Serial.write(stylusb1);
+      Serial.write(stylusb2);
+      Serial.flush();
+  } else if (serialMode == 1) {
       Serial.print(x_raw);
       Serial.print(" ");
       Serial.print(y_raw);
@@ -125,78 +132,49 @@ void sendData() {
       Serial.print(stylusb2);
       Serial.println("");
       Serial.flush();
-      while ((UCSR0A & _BV (TXC0)) == 0) {}
-      break;
-    case 1: // wait mode do nothing
-      break;
-    case 2:
-      break;
+  } else if (serialMode == 2) {
+      uint8_t firstByte = 0b10100000;
+      if (x_raw != 4095) firstByte += 0b01000000;
+      if (stylusb0 == 1 || stylusb1 == 1 || stylusb2 == 1) firstByte += 0b00001000;
+      firstByte += (x_raw >> 14) & 0b00000011;
+      uint8_t secondByte = (x_raw >> 7) & 0b01111111;
+      uint8_t thirdByte = x_raw & 0b01111111;
+      uint8_t fourthByte = (y_raw >> 14) & 0b00000011;
+      if (stylusb0 == 1) fourthByte += 0b00001000;
+      if (stylusb1 == 1) fourthByte += 0b00010000;
+      if (stylusb2 == 1) fourthByte += 0b00100000;
+      byte mappedPressure = map(pressure_raw, 8, 255, -128, 128) & 0xFF;
+      fourthByte += (mappedPressure << 2) & 0b00000100;
+      uint8_t fifthByte = (y_raw >> 7) & 0b01111111;
+      uint8_t sixthByte = y_raw & 0b01111111;
+      uint8_t seventhByte = mappedPressure >> 1;
+      seventhByte += (mappedPressure >> 1) && 0b01000000;
+      Serial.write(firstByte);
+      Serial.write(secondByte);
+      Serial.write(thirdByte);
+      Serial.write(fourthByte);
+      Serial.write(fifthByte);
+      Serial.write(sixthByte);
+      Serial.write(seventhByte);
+      Serial.flush();
+  } else {
+    
   }
 }
 
-// receive data via serial
-// DOES NOTHING right now but is the beginning of 
-// creating ISDV4 protocol support
-void receiveData() {
-  // incoming data (ISDV4)
-  // read from port 0, send to port 1:
-  if (Serial.available()) {
-    char inByte = Serial.read();
-    uint8_t reponseByte = 0;
-    switch(inByte) {
-      case '*': // ISDV4_QUERY - Query stylus configuration data (expects a response)
-        //Stylus Query Response
-        //Byte 	Bits 	Description
-        //0 	  7 	Always 1 -- marks beginning of packet
-	//        6 	Always 1 - indicates control packet
-	//        0-5 	Data ID
-        //1       7 	Always 0
-	//        0-6 	Max X (bits 7-13 of X coordinate)
-        //2 	  7 	Always 0
-	//        2-6 	Max X (bits 2-6 of X coordinate)
-        //3 	  7 	Always 0
-	//        0-6 	Max Y (bits 7-13 of Y coordinate)
-        //4 	  7 	Always 0
-	//        2-6 	Max Y (bits 2-6 of Y coordinate)
-        //5 	  7 	Always 0
-	//        0-6 	Max Pressure (bits 0-6 of Pressure).
-        //6 	  7 	Always 0
-	//        5-6 	Max X (bits 0-1 of X coordinate)
-	//        3-4 	Max Y (bits 0-1 of Y coordinate)
-	//        0-2 	Max Pressure (bits 7-9 of Pressure)
-        //7 	  7 	Always 0
-	//        0-6 	Max Y Tilt
-        //8 	  7 	Always 0
-	//        0-6 	Max X Tilt
-        //9 	  7 	Always 0
-	//        0-6 	Version (bits 14-7)
-        //10 	  7 	Always 0
-	//        0-6 	Version (bits 0-6)
-        
-        break;
-      case '%': // ISDV4_TOUCH_QUERY - Query touchscreen configuration data (expects a response)
-      
-        break;
-      case '0': // ISDV4_STOP - Stop sending coordinates
-      
-        break;
-      case '1': // ISDV4_START - Start sending coordinates
-      
-        break;
-      case '&': // ISDV4_RESET - Touch panel reset request
-      
-        break;
-    }
-  } 
-}
-
-// the main loop of course ;-)
 void loop() {
   getDataFromTablet();
-  sendData();
-  receiveData();
+  if (sending) sendData();
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\r');
+    if (command == "~#") Serial.print("~#UD-1212-R00,V1.2-7\r");
+    else if (command == "SP") sending = false;
+    else if (command == "ST") sending = true;
+    else if (command == "~C") Serial.print("~C02000,01500\r");
+    else if (command == "~R") Serial.print("~RE203A000,000,02,2000,1500");
+    else if (command == "SETSERIAL1") serialMode = 1;
+    else if (command == "SETSERIAL2") serialMode = 2;
+    else if (command == "SETSERIAL0") serialMode = 5;
+    else Serial.print(command);
+  }
 }
-
-
-
-
